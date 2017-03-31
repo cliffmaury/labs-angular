7b4b409d4468a0a4f2c2f30de099cb9a7d8c76fb
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-03-31 14:57:47 +0200

    Add material

---
+++ src/app/chrono/chrono.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chrono',
+  templateUrl: './chrono.component.html',
+  styleUrls: ['./chrono.component.css']
+})
+export class ChronoComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
