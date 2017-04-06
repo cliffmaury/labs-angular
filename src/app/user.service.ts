6b271de876805205e38e91448322482e158e9850
Théodore LUU <luu.theodore@gmail.com>
2017-04-06 02:10:57 +0200

    add busy status, add png images

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,11 +7,11 @@ export class UserService {
 
   private users = [
     {id: 1, email: 'john.doe@gmail.com', status: 'online'},
-    {id: 2, email: 'jane.doe@gmail.com', status: 'offline'},
-    {id: 3, email: 'jean.dupond@gmail.com', status: 'online'},
-    {id: 4, email: 'jean.dupont@gmail.com', status: 'online'},
+    {id: 2, email: 'jane.doe@gmail.com', status: 'online'},
+    {id: 3, email: 'jean.dupond@gmail.com', status: 'busy'},
+    {id: 4, email: 'jean.dupont@gmail.com', status: 'offline'},
     {id: 5, email: 'jeanne.dupond@gmail.com', status: 'offline'},
-    {id: 5, email: 'joe.doe@gmail.com', status: 'offline'}
+    {id: 6, email: 'joe.doe@gmail.com', status: 'online'}
   ];
 
   get() {
