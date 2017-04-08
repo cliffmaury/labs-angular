9721b10a64a375bbb7007322cfca1ffc1fd3e5b5
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-04-08 16:10:24 +0200

    Update design and sources

--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -1,33 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from "../user.service";
-
-const CLICK_TO_HIDE_MESSAGE:string = "Cliquez sur ce texte pour cacher les utilisateurs hors ligne";
-const CLICK_TO_SHOW_MESSAGE:string = "Cliquez sur ce texte pour afficher les utilisateurs hors ligne";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-user-table',
-  templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.css'],
+    selector: 'app-user-table',
+    templateUrl: './user-table.component.html',
+    styleUrls: ['./user-table.component.css'],
 })
 export class UserTableComponent implements OnInit {
 
-  constructor(private _userService: UserService) { }
+    constructor(private _userService: UserService) { }
 
-  public users;
-  public hideOffline: boolean = false;
-  public captionMsg: String = CLICK_TO_HIDE_MESSAGE;
+    public users;
+    public hideOffline: boolean = false;
 
-  ngOnInit() {
-    this.users = this._userService.get();
-  }
+    ngOnInit() {
+        this.users = this._userService.get();
+    }
 
-  showHideOfflineUser() {
-    this.hideOffline = !this.hideOffline;
-    this.captionMsg = !this.hideOffline ? CLICK_TO_HIDE_MESSAGE : CLICK_TO_SHOW_MESSAGE;
-  }
+    showHideOfflineUser() {
+        this.hideOffline = !this.hideOffline;
+    }
 
-  trackByUserId(index, user) {
-    return user.id
-  }
+    trackByUserId(index, user) {
+        return user.id
+    }
 
 }
