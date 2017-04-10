74c33d0a1635f16c17de5b4baf13724f4cd99a43
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-04-10 23:05:35 +0200

    Remove file

---
+++ test/rest.spec.ts
@@ -0,0 +1,14 @@
+
+import {expect} from "chai";
+import {$log} from "ts-log-debug";
+
+describe('Rest :', () => {
+
+
+    it('should do nothing', () => {
+
+        expect(true).to.be.true;
+
+    });
+
+});
