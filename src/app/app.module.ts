2bc6bfdffd94d7886ea33602ce0da99b895d427f
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-04-11 14:09:11 +0200

    Fix version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import 'hammerjs';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -16,8 +17,9 @@ import {DecToStrPipe} from "app/dec-to-str.pipe";
         DecToStrPipe
     ],
     imports: [
-        MaterialModule,
         BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModule,
         FormsModule,
         HttpModule
     ],
