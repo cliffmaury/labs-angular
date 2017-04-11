bf9b4fa48e35f80c2902415b51f4d42a08311a6a
Romain Lenzotti <rom.lenzotti@gmail.com>
2017-04-11 10:37:59 +0200

    Fix material and socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import 'hammerjs';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { UserService } from './user.service';
 import { UserTableComponent } from './user-table/user-table.component';
 import { UserTableRowComponent } from './user-table-row/user-table-row.component';
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {SignUpComponent} from "./sign-up/sign-up.component";
-import {ROUTES} from "./app.routes";
-import {AuthenticationService} from "./authentication.service";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { ROUTES } from "./app.routes";
+import { AuthenticationService } from "./authentication.service";
 import { DialogUserComponent } from './dialog-user/dialog-user.component';
-import {RouterModule} from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
@@ -30,8 +33,9 @@ import { NavbarComponent } from './navbar/navbar.component';
         NavbarComponent
     ],
     imports: [
-        MaterialModule,
         BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModule,
         FormsModule,
         HttpModule,
         RouterModule.forRoot(ROUTES)
